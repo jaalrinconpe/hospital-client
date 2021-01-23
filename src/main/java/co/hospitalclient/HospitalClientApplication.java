49184d10a846c6985e7390ebbfc22d0a21e3561e
Javier Alexander Rincon <javiernk118@gmail.com>
2021-01-22 19:46:49 -0500

    Commit inicial

---
+++ src/main/java/co/hospitalclient/HospitalClientApplication.java
@@ -0,0 +1,13 @@
+package co.hospitalclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalClientApplication.class, args);
+	}
+
+}
